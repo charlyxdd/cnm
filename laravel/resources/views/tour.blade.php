947990369baa79f6113dfc8828d5c298e6de46d4
Juan Carlos Tzuc <jtzuc@Mini-de-Luis.lan>
2016-11-30 13:18:37 -0500

    pagina de tour terminada

--- laravel/resources/views/tour.blade.php
+++ laravel/resources/views/tour.blade.php
@@ -2,6 +2,9 @@
 
 @section('css')
     <link href="<?php echo url('/css/tour.css');?>" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{url('/css/normalize.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{url('/css/demo.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{url('/css/component.css')}}" />
 @endsection
 
 @section('cuerpo')
@@ -56,10 +59,186 @@
                 </div>
             </div>
 
+            <div class="container">
+                <div class="infoContainer">
+                    <p>
+                        Once we return, the guided portion of the tour would be over, If you are feeling a little more
+                        adventurous, you are more than welcome to take the scooter and do a little more exploring yourself,
+                        there are a few sites downtown that you don’t want to miss, like the Corpus Christi church, the local
+                        Market and the Local Museum.
+                    </p>
+                </div>
+                <div class="imagenContainer">
+                    <img src="{{url('/images/Scooter Tour/8.png')}}">
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="imagenContainer">
+                    <img src="{{url('/images/Scooter Tour/9.png')}}">
+                </div>
+                <div class="infoContainer">
+                    <p>
+                        The tour will last about 4-5 hours depending upon the weather.  The scooters need to be returned no
+                        later 4:30pm
+                    </p>
+                </div>
+            </div>
+
+
+            <div id="funContainer">
+                <p>Sin preocupaciones</p>
+            </div>
+
+            <div id="puntosContainer">
+                <div>
+                    <h3>Princig Includes:</h3>
+                    <ul>
+                        <li>Tour Guide (Bilingual English/Spanish)</li>
+                        <li>100% covered for insurance during the tour only, after the tour a $250.00 deductible will be
+                            added
+                        </li>
+                        <li>Snorkeling equipment with guide</li>
+                        <li>Lunch (nonalcoholic drinks & tips not included</li>
+                        <li>Bottle of water at each location around the island</li>
+                        <li>Gas (other rental locations charge to fill up)</li>
+                        <li>$80.00 1 Scooter 1 Person</li>
+                        <li>$120.00 1 Scooter 2 person</li>
+
+                        *Please book in advance beacuase we have limited numer of scooters.
+                    </ul>
+                </div>
+                <div>
+                    <h3>Recommendations:</h3>
+                    <ul>
+                        <li>Sunblock</li>
+                        <li>Sunglasses</li>
+                        <li>Mosquito repellent</li>
+                        <li>Towels</li>
+                        <li>Change of clothes</li>
+                        <li>Comfortable shoes</li>
+                        <li>Camera</li>
+                        <li>Extra spending money</li>
+                        <li>Hat</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Important things to remember:</h3>
+                    <ul>
+                        <li>Helmets must worn at all times by ALL Guests</li>
+                        <li>Must have valid Driver's licences, expired licenses are NOT VALID</li>
+                        <li>Must be 18 years or older to drive</li>
+                        <li>Children under the age of 8 will not be allowed on the snorkel tour</li>
+                        <li>This tour is no recommended for people who have neck/back injuries or women are pregnant
+                        </li>
+                        <li>Driving under the influence of alcohol or illegal substances is forbidden and your insurance
+                            will become null and void
+                        </li>
+                        <li>Inclement weather, some streets that are lined with bricks please use caution as they become
+                            slippey
+                        </li>
+                        <li>Please watch for sign that state "alto" which means stop</li>
+                        <li>Most streets are one way, please watch that you are going in the right direction</li>
+                        <li>Slow down at all "topes" which means speed bump, they are located accross the island</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div id="galeriaContainer">
+                <div id="tituloContainer">
+                    <h2>Galeria</h2>
+                </div>
+                <section id="photostack-1" class="photostack phorostack-start">
+                    <div>
+                        <figure>
+                            <a class="photostack-img">
+                                <img src="{{ url('/images/Scooter tour/galeria/1.jpg') }}" alt="img02"/>
+                            </a>
+                            <figcaption>
+                                <h2 class="photostack-title">Harley Davidson</h2>
+                                <!-- optional backside -->
+                                <div class="photostack-back">
+                                    <p></p>
+                                </div>
+                            </figcaption>
+                        </figure>
+
+
+                        <figure>
+                            <a class="photostack-img">
+                                <img src="{{ url('/images/Scooter tour/galeria/2.jpg') }}" alt="img02"/>
+                            </a>
+                            <figcaption>
+                                <h2 class="photostack-title">Harley Davidson</h2>
+                                <!-- optional backside -->
+                                <div class="photostack-back">
+                                    <p></p>
+                                </div>
+                            </figcaption>
+                        </figure>
+
+                        <figure>
+                            <a class="photostack-img">
+                                <img src="{{ url('/images/Scooter tour/galeria/3.jpg') }}" alt="img02"/>
+                            </a>
+                            <figcaption>
+                                <h2 class="photostack-title">Scooter Tour</h2>
+                                <!-- optional backside -->
+                                <div class="photostack-back">
+                                    <p></p>
+                                </div>
+                            </figcaption>
+                        </figure>
+
+                        <figure>
+                            <a class="photostack-img">
+                                <img src="{{ url('/images/Scooter tour/galeria/4.jpg') }}" alt="img02"/>
+                            </a>
+                            <figcaption>
+                                <h2 class="photostack-title">Scooter Tour</h2>
+                                <!-- optional backside -->
+                                <div class="photostack-back">
+                                    <p></p>
+                                </div>
+                            </figcaption>
+                        </figure>
+
+                        <figure>
+                            <a class="photostack-img">
+                                <img src="{{ url('/images/Scooter tour/galeria/5.jpg') }}" alt="img02"/>
+                            </a>
+                            <figcaption>
+                                <h2 class="photostack-title">Scooter Tour</h2>
+                                <!-- optional backside -->
+                                <div class="photostack-back">
+                                    <p></p>
+                                </div>
+                            </figcaption>
+                        </figure>
+
+                        <figure>
+                            <a class="photostack-img">
+                                <img src="{{ url('/images/Scooter tour/galeria/6.jpg') }}" alt="img02"/>
+                            </a>
+                            <figcaption>
+                                <h2 class="photostack-title">Scooter and Harley Davidson</h2>
+                                <!-- optional backside -->
+                                <div class="photostack-back">
+                                    <p></p>
+                                </div>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </section>
+            </div>
+
         </div>
     </div>
 @endsection
 
 @section('js')
-    <script src="{{ url('/js/tours.js') }}"></script>
+    <script src="{{url('/js/modernizr.min.js')}}"></script>
+    <script src="{{url('/js/classie.js')}}"></script>
+    <script src="{{url('/js/photostack.js')}}"></script>
+    <script src="{{ url('/js/tour.js') }}"></script>
 @endsection
---
+++ public_html/js/tour.js
@@ -0,0 +1,5 @@
+new Photostack( document.getElementById( 'photostack-1' ), {
+    callback : function( item ) {
+        //console.log(item)
+    }
+} );
